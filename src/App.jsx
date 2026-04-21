975ef6e0671cf78814685f3bb73869bd4d0da144
PatS4050 <patrick@straatman.eu>
2026-04-21 16:39:55 +0200

    opdracht 1 gemaakt

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import './App.css'
 
+const bTest1 = "Test 1 completed"
+const bTest2 = "Test 2 completed"
+const bTest3 = "Test 3 completed"
+
+
 function App() {
-  return (
-      <h1>Handbags & Purses</h1>
-  )
+    function buttonClick(e) {
+        console.log(e)
+    }
+    return (
+        <>
+            <nav>
+          <span>
+              <button type="button" name="buttonTest1" onClick={() => buttonClick(bTest1)}>test 1</button>
+              <button type="button" name="buttonTest1" onClick={() => buttonClick(bTest2)}>test 2</button>
+              <button type="button" name="buttonTest1" onClick={() => buttonClick(bTest3)} disabled>test 3</button>
+          </span>
+            </nav>
+            <h1>Handbags & Purses</h1>
+        </>
+    )
 }
 
 export default App
---
+++ src/component/button/Button.jsx
@@ -0,0 +1,7 @@
+import './App.css'
+import React from 'react';
+
+function Button ({buttonName, logText, }) {
+
+}
+export default Button
