7d9be9204b7485e39b918305a85ced0c455dddbd
PatS4050 <patrick@straatman.eu>
2026-04-21 22:11:09 +0200

    opgave 1, knop disabled nog niet goed, begonnen aan 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
 import './App.css'
-
-const bTest1 = "Test 1 completed"
-const bTest2 = "Test 2 completed"
-const bTest3 = "Test 3 completed"
-
+import Button from "./component/button/Button.jsx";
 
 function App() {
-    function buttonClick(e) {
-        console.log(e)
-    }
+
     return (
         <>
-            <nav>
-          <span>
-              <button type="button" name="buttonTest1" onClick={() => buttonClick(bTest1)}>test 1</button>
-              <button type="button" name="buttonTest1" onClick={() => buttonClick(bTest2)}>test 2</button>
-              <button type="button" name="buttonTest1" onClick={() => buttonClick(bTest3)} disabled>test 3</button>
-          </span>
-            </nav>
             <h1>Handbags & Purses</h1>
+            <nav>
+                <span>
+                    <Button label="to the collection" name="test 1" logText="Test 1 is completed"/>
+                     <Button label="shop all bags" name="test 2" logText="Test 2 is completed" disabled={false}/>
+                     <Button label="pre-orders" name="test 3" logText="Test 3 is completed" disabled= {true}  />
+                </span>
+            </nav>
+            <main className="products">
+                <span>
+                    <article/>
+                    <article/>
+                    <article/>
+                    <article/>
+                </span>
+            </main>
+
         </>
     )
 }
--- src/component/button/Button.jsx
+++ src/component/button/Button.jsx
@@ -1,7 +1,22 @@
-import './App.css'
+import './button.css'
 import React from 'react';
 
-function Button ({buttonName, logText, }) {
+//
+// function Button (props) {
+//     return (
+//         <button type="button">test </button>
+//     )}
 
-}
+
+
+
+
+
+function Button ({ label, logText, name, disabled }) {
+    function buttonClick(e) {
+        console.log(e)
+    }
+    return (
+    <button type="button" name={name} onClick={() => buttonClick(logText)} disabled={false} >{label}</button>
+    )}
 export default Button
